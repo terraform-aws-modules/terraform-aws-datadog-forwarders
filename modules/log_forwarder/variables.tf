87ad3963fa4bfb6b3e634afd5b00cdb297b6b5d2
Bryant Biggs <bryantbiggs@gmail.com>
2022-03-05 09:04:47 -0500

    feat: add new release v3.43.0 and set as the default version - [ref](https://github.com/DataDog/datadog-serverless-functions/releases/tag/aws-dd-forwarder-3.43.0)

--- modules/log_forwarder/variables.tf
+++ modules/log_forwarder/variables.tf
@@ -182,7 +182,7 @@ variable "s3_log_bucket_arns" {
 variable "forwarder_version" {
   description = "Forwarder version - see https://github.com/DataDog/datadog-serverless-functions/releases"
   type        = string
-  default     = "3.42.0"
+  default     = "3.43.0"
 }
 
 variable "name" {
--- variables.tf
+++ variables.tf
@@ -187,7 +187,7 @@ variable "log_forwarder_s3_log_bucket_arns" {
 variable "log_forwarder_version" {
   description = "Forwarder version - see https://github.com/DataDog/datadog-serverless-functions/releases"
   type        = string
-  default     = "3.42.0"
+  default     = "3.43.0"
 }
 
 variable "create_log_forwarder" {
@@ -369,7 +369,7 @@ variable "rds_em_forwarder_policy_path" {
 variable "rds_em_forwarder_version" {
   description = "RDS enhanced monitoring lambda version - see https://github.com/DataDog/datadog-serverless-functions/releases"
   type        = string
-  default     = "3.42.0"
+  default     = "3.43.0"
 }
 
 variable "create_rds_em_forwarder" {
@@ -563,7 +563,7 @@ variable "vpc_fl_forwarder_read_cloudwatch_logs" {
 variable "vpc_fl_forwarder_version" {
   description = "VPC flow log lambda version - see https://github.com/DataDog/datadog-serverless-functions/releases"
   type        = string
-  default     = "3.42.0"
+  default     = "3.43.0"
 }
 
 variable "create_vpc_fl_forwarder" {
