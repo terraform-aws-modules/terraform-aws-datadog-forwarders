15148f363125fd166a4fde44ef9fe7d98881bd34
Michi U <4169888+michemache@users.noreply.github.com>
2023-02-03 16:03:05 +0100

    update s3-bucket module to v3.6.1

--- modules/log_forwarder/main.tf
+++ modules/log_forwarder/main.tf
@@ -24,7 +24,7 @@ data "aws_region" "current" {}
 
 module "this_s3_bucket" {
   source  = "terraform-aws-modules/s3-bucket/aws"
-  version = "v3.3.1"
+  version = "v3.6.1"
 
   create_bucket = var.create && var.create_bucket
   bucket        = local.bucket_name
