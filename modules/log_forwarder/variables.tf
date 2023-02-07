51ce957855dc699765d1ba9ebb15c2d6e6598a35
Bryant Biggs <bryantbiggs@gmail.com>
2023-02-07 08:22:39 -0500

    feat: Add latest lambda archives using `vendor.sh` and set default version to `3.69.0`

--- modules/log_forwarder/variables.tf
+++ modules/log_forwarder/variables.tf
@@ -186,7 +186,7 @@ variable "s3_log_bucket_arns" {
 variable "forwarder_version" {
   description = "Forwarder version - see https://github.com/DataDog/datadog-serverless-functions/releases"
   type        = string
-  default     = "3.44.0"
+  default     = "3.69.0"
 }
 
 variable "name" {
