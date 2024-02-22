17cebbb7928c26568d9e814a49eca3d35fdb7a53
Bryant Biggs <bryantbiggs@gmail.com>
2024-02-22 18:32:19 -0500

    fix: Downgrade VPC module version for < 1.0 Terraform

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -237,7 +237,7 @@ resource "aws_kms_alias" "datadog" {
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 5.0"
+  version = "~> 4.0"
 
   name = local.name
   cidr = local.vpc_cidr
@@ -253,7 +253,7 @@ module "vpc" {
 
 module "vpc_endpoints" {
   source  = "terraform-aws-modules/vpc/aws//modules/vpc-endpoints"
-  version = "~> 5.0"
+  version = "~> 4.0"
 
   vpc_id             = module.vpc.vpc_id
   security_group_ids = [module.security_group.security_group_id]
